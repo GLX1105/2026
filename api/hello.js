d206d5714781593efd119612ff63cc0bbd748c1e
GLX1105 <562761508@qq.com>
2026-08-06 03:51:01 +0800

    Delete api directory

--- api/hello.js
+++
@@ -1,7 +0,0 @@
-export default function handler(req, res) {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.status(200).json({
-    message: '后端运行成功！',
-    time: new Date().toISOString()
-  });
-}
